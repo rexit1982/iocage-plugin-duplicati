16a274717bcc37a0851cfd42d7bc73299cf56e47
rexit1982 <rexit1982@gmail.com>
2020-02-16 23:50:41 -0500

    add sleep to restart
    add sleep to restart script, service command stops and tries to start a little too fast for the child processes to get cleaned up when applying settings.  Update notes left over by post_install script.

--- post_install.sh
+++ post_install.sh
@@ -25,6 +25,10 @@ chown -R duplicati:duplicati $duplicati_dir
 chown -R duplicati:duplicati $duplicati_conf_dir
 
 echo "Default Web password: duplicati" > /root/PLUGIN_INFO
+echo "Default web port: 8200" >> /root/PLUGIN_INFO
+echo "Service user and group: duplicati" >> /root/PLUGIN_INFO
+echo "To change settings until the UI is fixed use the command line.  Example: iocage get -P port PLUGIN" >> /root/PLUGIN_INFO
+echo "Configurable settings are port and webpass." >> /root/PLUGIN_INFO
 
 #Thank you Asigra via ConorBeh for this hack
 echo "Find Network IP"
@@ -38,3 +42,7 @@ service duplicati start
 echo "------Plugin Info------"
 echo "Access Web interface to configure http://${IP}:8200"
 echo "Default Web Password: duplicati"
+echo "Default web port: 8200"
+echo "Service user and group: duplicati" 
+echo "To change settings until the UI is fixed use the command line.  Example: iocage get -P port PLUGIN"
+echo "Configurable settings are port and webpass." 
